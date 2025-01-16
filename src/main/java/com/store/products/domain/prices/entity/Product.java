73e943ec3994717035b28eaddff78bff5758ad80
Ganexy <ganexygan@hotmail.com>
2025-01-16 15:19:17 +0100

    Architecture structure and some bases

---
+++ src/main/java/com/store/products/domain/prices/entity/Product.java
@@ -0,0 +1,89 @@
+package com.store.products.domain.prices.entity;
+
+import java.time.LocalDateTime;
+
+public class Product {
+    private String brandId;
+    private String priceList;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String productId;
+    private String priority;
+    private String price;
+    private String currency;
+
+    public Product(String brandId, String priceList, LocalDateTime startDate, LocalDateTime endDate, String productId, String priority, String price, String currency) {
+        this.brandId = brandId;
+        this.priceList = priceList;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.productId = productId;
+        this.priority = priority;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getPriceList() {
+        return priceList;
+    }
+
+    public void setPriceList(String priceList) {
+        this.priceList = priceList;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
